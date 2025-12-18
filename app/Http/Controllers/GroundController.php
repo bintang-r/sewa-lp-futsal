7ff608de61a22bcc643c961b4516fdc395a2f428
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-12-19 01:14:08 +0800

    chore: scheme for (model, migration, controller and unit test)

---
+++ app/Http/Controllers/GroundController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ground;
+use Illuminate\Http\Request;
+
+class GroundController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ground $ground)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Ground $ground)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Ground $ground)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ground $ground)
+    {
+        //
+    }
+}
